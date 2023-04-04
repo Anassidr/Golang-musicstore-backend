100756e4368cfe04ae26aede55138200d1793b56
Anassidr <kamalanass96@gmail.com>
2023-04-04 16:49:03 +0200

    0404

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 )
 
 require (
--- pkg/models/music.go
+++ pkg/models/music.go
@@ -5,6 +5,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *gorm.DB
@@ -56,4 +57,30 @@ func DeleteInstrument(Id int64) Instrument {
 	var instrument Instrument
 	db.Where("ID=?", Id).Delete(instrument)
 	return instrument
+}
+
+type User struct {
+	gorm.Model
+	Name               string `json:"Name" valid:"required"`
+	Address            string `json:"address" valid:"required"`
+	Username           string `json:"username" valid:"required"`
+	Email              string `json:"email" valid:"required,email"`
+	Email_verification string `json:"email_verification" valid:"required"`
+	PasswordHash       string `json:"-"` // not exposed in JSON responses
+	Role               string `json:"role" valid:"required"`
+	Gender             string `json:"qty_stock" valid:"required,numeric,min=0"`
+}
+
+func (u *User) SetPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.PasswordHash = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+	return err == nil
 }
